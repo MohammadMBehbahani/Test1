6b860ead23156569247dc9515cc09988c4d79a52
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-22 20:38:24 +0330

    Add Fish Entity Tests

---
+++ Tests/FishManagement_Domain_Tests/Entities_Tests/Fish_Tests/Fish.cs
@@ -0,0 +1,14 @@
+﻿namespace FishManagement_Domain_Tests.Entities_Tests.Fish_Tests
+{
+    internal class Fish
+    {
+        private string fishType;
+        private double fishFoodWeight;
+
+        public Fish(string fishType, double fishFoodWeight)
+        {
+            this.fishType = fishType;
+            this.fishFoodWeight = fishFoodWeight;
+        }
+    }
+}
---
+++ Tests/FishManagement_Domain_Tests/Entities_Tests/Fish_Tests/FishEntity_Tests.cs
@@ -0,0 +1,20 @@
+﻿namespace FishManagement_Domain_Tests.Entities_Tests.Fish_Tests;
+
+[TestFixture]
+public class FishEntity_Tests
+{
+
+
+    [Test]
+    public void Fish_CreateFishEntity_CreateNewInstanceOfFishEntity()
+    {
+        //Arrage
+        var fishType = "gold";
+        var fishFoodWeight = 0.1;
+        //Act
+        var fishEntity = new Fish(fishType, fishFoodWeight);
+        //Assert
+        Assert.NotNull(fishEntity);
+
+    }
+}
