1997dab5a61685a7ba1556a611be07bdad5eb681
MohamadSoheil Davoody <cap.m.davoodi@outlook.com>
2023-02-22 20:12:01 +0330

    Added tank entity test for create new tank and don't get any exception

---
+++ Tests/FishManagement_Domain_Tests/Entities_Tests/Tank_Tests/Tank.cs
@@ -0,0 +1,12 @@
+﻿namespace FishManagement_Domain_Tests.Entities_Tests.Tank_Tests
+{
+    internal class Tank
+    {
+        private string tankName;
+
+        public Tank(string tankName)
+        {
+            this.tankName = tankName;
+        }
+    }
+}
---
+++ Tests/FishManagement_Domain_Tests/Entities_Tests/Tank_Tests/TankEntity_Tests.cs
@@ -0,0 +1,18 @@
+﻿namespace FishManagement_Domain_Tests.Entities_Tests.Tank_Tests;
+
+[TestFixture]
+public class TankEntity_Tests
+{
+
+
+    [Test]
+    public void Tank_CreateTankEntityWithoutAnyException_ShoulReturnNewInstanseOfTankEntity()
+    {
+        //Arrage
+        string tankName = "NewTank";
+        //Act
+        var tankInstanse = new Tank(tankName);
+        //Assert
+        Assert.NotNull(tankInstanse);
+    }
+}
